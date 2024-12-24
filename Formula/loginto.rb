2138ed3d50ba18db34dfc03ed440fa579dfa72ef
Yang Hanlin <mattoncis@hotmail.com>
2024-12-24 21:28:52 +0800

    test

--- Formula/loginto.rb
+++ Formula/loginto.rb
@@ -1,45 +1,31 @@
 class Loginto < Formula
   desc "Log into remote hosts with convenience"
   homepage "https://github.com/yanghanlin/tdutilities/tree/main/loginto"
-  version "v0.0"
+  version "0.0-40.2165ec5"
 
   on_macos do
     on_intel do
-      url "https://static.tree-diagram.site/utilities/loginto-darwin-amd64-latest"
-      sha256 "invalid"
-
-      def install
-        bin.install "loginto-darwin-amd64-latest" => "loginto"
-      end
+      url "https://static.tree-diagram.site/utilities/loginto_0.0-40.2165ec5_Darwin_x86_64.tar.xz"
+      # sha256 "invalid"
     end
-
     on_arm do
-      url "https://static.tree-diagram.site/utilities/loginto-darwin-arm64-latest"
-      sha256 "invalid"
-
-      def install
-        bin.install "loginto-darwin-arm64-latest" => "loginto"
-      end
+      url "https://static.tree-diagram.site/utilities/loginto_0.0-40.2165ec5_Darwin_arm64.tar.xz"
+      # sha256 "invalid"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://static.tree-diagram.site/utilities/loginto-linux-amd64-latest"
-      sha256 "invalid"
-
-      def install
-        bin.install "loginto-linux-amd64-latest" => "loginto"
-      end
+      url "https://static.tree-diagram.site/utilities/loginto_0.0-40.2165ec5_Linux_x86_64.tar.xz"
+      # sha256 "invalid"
     end
-
     on_arm do
-      url "https://static.tree-diagram.site/utilities/loginto-linux-arm64-latest"
-      sha256 "invalid"
-
-      def install
-        bin.install "loginto-linux-arm64-latest" => "loginto"
-      end
+      url "https://static.tree-diagram.site/utilities/loginto_0.0-40.2165ec5_Linux_aarch64.tar.xz"
+      # sha256 "invalid"
     end
+  end
+
+  def install
+    prefix.install Dir["*"]
   end
 end
